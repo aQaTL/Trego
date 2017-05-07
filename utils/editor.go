9198ee0b18064228e74a09065861bfcd8323ad86
aQaTL <mmsoltys@outlook.com>
2017-05-07 22:20:33 +0200

    CursorUp and CursorDown now respects wrapped lines

--- utils/editor.go
+++ utils/editor.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-//Moves cursor in list one line up
+//Moves cursor in the view one line up
 func CursorUp(g *Gui, v *View) (err error) {
 	if v != nil {
 		cx, cy := v.Cursor()
@@ -16,18 +16,32 @@ func CursorUp(g *Gui, v *View) (err error) {
 		if oy > 0 && cy == 0 {
 			err = v.SetOrigin(ox, oy-1)
 		}
+
+		viewBuffer := strings.Split(v.ViewBuffer(), "\n")
+		if len(viewBuffer) > 0 && cy-1 >= 0 {
+			if line := viewBuffer[cy-1]; len(line) > 0 && (line[0] < 0x30 || line[0] > 0x39) {
+				return CursorUp(g, v)
+			}
+		}
 	}
 	return
 }
 
-//Moves cursor in list one line down
+//Moves cursor in the view one line down
 func CursorDown(g *Gui, v *View) (err error) {
 	if v != nil {
 		cx, cy := v.Cursor()
 		ox, oy := v.Origin()
-		if cy+oy < (len(strings.Split(v.ViewBuffer(), "\n")) - 3) {
+		viewBuffer := strings.Split(v.ViewBuffer(), "\n")
+		if cy+oy < (len(viewBuffer) - 3) {
 			if err = v.SetCursor(cx, cy+1); err != nil {
 				err = v.SetOrigin(ox, oy+1)
+			}
+		}
+		if len(viewBuffer) > 0 {
+			line := viewBuffer[cy+1]
+			if cy+1 != len(viewBuffer) && len(line) > 0 && (line[0] < 0x30 || line[0] > 0x39) {
+				return CursorDown(g, v)
 			}
 		}
 	}
